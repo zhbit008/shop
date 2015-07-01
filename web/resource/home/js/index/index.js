7b8bdb04dfd119ea3991c6786ce1d77dea6afc46
zhbitcxy <542610267@qq.com>
2015-07-01 23:17:11 +0800

    异步分页新增缓存超时处理

--- web/resource/home/js/index/index.js
+++ web/resource/home/js/index/index.js
@@ -11,53 +11,76 @@ $(function(){
      * 分页对象
      * @type {{cache: Array, url: string, data: {p: number, num: number, cid: number}, success: success, get: get}}
      */
-    var pageHandle = {
-        cache : [],
-        cachefield : '',
-        url: '/ajax/Home_product_getPage',
-        data: {'p':1,'num':9, 'cid': 0},
-        success : function(json){
+//        TODO  需要能设置初始值
+    var pageHandle = (function(){
+
+        var _page = {
+            timeout : 60, //s
+            cache : [],
+            cacheField : '',
+            url: '/ajax/Home_product_getPage',
+//            TODO 这里的参数不够通用
+            data: {'p':1,'num':9, 'cid': 0},
+            success : function(json){
 //            console.log(json);
-            var list = json.data.list;
-            var page = json.data.page;
-            var _this = this;
-            //缓存
-            pageHandle.cache[pageHandle.cachefield] = json;
-            //清空产品
-            $boxes.empty();
-            //重新生成物品
-            for(var i = 0; i < list.length; i++){
-                var $item = $box.clone(true);
-                $item.find('.proname').text(list[i].proname);
-                $item.find('.title').text(list[i].title);
-                $item.find('.url').attr('href','/home/product_show.html?productId='+list[i].id);
-                $item.find('.img').attr('src', '/upload/product/'+list[i].image+'.jpg');
-                $boxes.append($item);
+                var list = json.data.list;
+                var page = json.data.page;
+                var _this = this;
+                //缓存
+                _page.cache[_page.cacheField] = {
+                    'json' : json,
+                    'time' : new Date().getTime()
+                };
+                //清空产品
+                $boxes.empty();
+                //重新生成物品
+                for(var i = 0; i < list.length; i++){
+                    var $item = $box.clone(true);
+                    $item.find('.proname').text(list[i].proname);
+                    $item.find('.title').text(list[i].title);
+                    $item.find('.url').attr('href','/home/product_show.html?productId='+list[i].id);
+                    $item.find('.img').attr('src', '/upload/product/'+list[i].image+'.jpg');
+                    $boxes.append($item);
+                }
+                //显示特效
+                $boxes.children().slideDown(800);
+                //删除分页
+                $page.empty();
+                //重新生成分页
+                $page.append(page);
+            },
+            //p为第几页，cid为分类id
+            get : function (p, cid, num) {
+                var _this = this;
+                _this.data.cid = cid || _this.data.cid;
+                _this.data.num = num || _this.data.num;
+                _this.data.p = p;
+                var _url = _this.url;
+                var _data = _this.data;
+                //获取缓存
+                _this.cacheField = _data.p+'_'+_data.num+'_'+_data.cid;
+                var _cache = _this.cache[_this.cacheField];
+
+                if (_cache){
+                    //缓存超时处理
+                    var  now = new Date().getTime();
+                    if (now - _cache.time > _this.timeout*1000 ) {
+                        //超时删除
+                        console.log("删除缓存");
+                        _this.cache[_this.cacheField] = null;
+                        $.post(_url,_data ,_this.success);
+                    }else{
+                        console.log("读取缓存")
+                        _this.success(_cache.json);
+                    }
+                }
+                else  { $.post(_url,_data ,_this.success); console.log("首次执行")}
+                return;
             }
-            //显示特效
-            $boxes.children().slideDown(800);
-            //删除分页
-            $page.empty();
-            //重新生成分页
-            $page.append(page);
-        },
-        //p为第几页，cid为分类id
-        get : function (p, cid, num) {
-            var _this = this;
-            _this.data.cid = cid || _this.data.cid;
-            _this.data.num = num || _this.data.num;
-            _this.data.p = p;
-            var _url = _this.url;
-            var _data = _this.data;
-            //获取缓存
-           _this.cachefield = _data.p+'_'+_data.num+'_'+_data.cid;
-            var _cache = _this.cache[_this.cachefield];
-//            TODO 增加缓存超时处理
-            if (_cache){ _this.success(_cache); console.log('cache data') }
-            else  { $.post(_url,_data ,_this.success); }
-            return;
-        }
-    };
+        };
+        return _page;
+    })();
+
     /**
      * 分页为异步分页
      */
